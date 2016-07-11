ba813938ba34d4935b00d5232c14ab283f5ed51e
paulBeaudet <inof8or@gmail.com>
2016-07-11 16:10:53 -0400

    Make sure connection comes back alive if it is unplugged for any reason

--- Firmware/nodeRelay/nodeRelay.js
+++ Firmware/nodeRelay/nodeRelay.js
@@ -2,15 +2,12 @@
 // This code lives on the linux side of an arduino yun
 // relaying card information from on board arduino to our access control sever via socket.io
 
+var RETRY_DELAY = 5000; // when to try again if no connection
 
 var socket = {
     io: require('socket.io-client')(process.env.SERVER),
-    init: function(){
-        socket.io.on('auth', function(data){
-            var pkg = "<" + data + ">"; // make sure arduino has start and end chars to read
-            console.log('send to arduino:' + pkg);
-            arduino.connection.write(pkg);
-        });
+    init: function(){ // notify authorization or denial: make sure arduino has start and end chars to read
+        socket.io.on('auth', function(data){ arduino.connection.write("<" + data + ">"); });
     }  
 }
 
@@ -29,15 +26,19 @@ var arduino = {
         arduino.connection.on('close', arduino.close);
         arduino.connection.on('error', arduino.error);
     },
-    open: function(){console.log('port open');},
+    open: function(){},
     read: function(data){
-        console.log(data);
         data = data.slice(0, data.length-1); // exclude newline char
         socket.io.emit('auth', {machine: process.env.MACHINE_NAME, card: data});
     },
-    close: function(){console.log('port closed');},
-    error: function(error){console.log('error:' + error);},
+    close: function(){
+        arduino.init();                        // try to re-establish if port was closed
+    },
+    error: function(error){                    // given something went wrong try to re-establish connection
+        setTimeout(arduino.init, RETRY_DELAY); // retry every half a minute
+    },
 }
 
 socket.init();
 arduino.init();
+
