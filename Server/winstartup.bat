8622c7cb0cfedf84605efc8a5231311a62d25f47
Steven Flagg Jr <sflaggjr@gmail.com>
2016-09-27 14:07:10 -0400

    Updated comments

--- Server/winstartup.bat
+++ Server/winstartup.bat
@@ -1,5 +1,7 @@
+::This script will setup the enviroment for the accessBot application
+::Before running this script, please run winsetup.cmd to ensure that you have the proper node modules
+::Please modify the SET values to the values related to your environment
 @ECHO OFF
-::
 CLS
 SET port=3000
 SET SESSION_SECRET=putyoursupperawesomesecretrighthere
